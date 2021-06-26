ee656f410a3300df9e72f0e1e27a05f98d99bad5
kaushik592k <kaushikbhartiya592k@gmail.com>
2021-06-27 04:37:36 +0530

    Frontend Starter

---
+++ frontend/index.js
@@ -0,0 +1,20 @@
+$(document).ready(function () {  
+    $("#txtCountry").autocomplete({  
+        source: function (request, response) {  
+            $.ajax({  
+                url: "http://localhost:8080/search",  
+                method: "post",  
+                contentType: "application/json;charset=utf-8",  
+                data: JSON.stringify({ term: request.term }),  
+                dataType: 'json',  
+                success: function (data) {  
+                    response(data.d);  
+                },  
+                error: function (err) {  
+                    alert(err);  
+                }  
+            });  
+            console.log(request);
+        }  
+    });  
+});
