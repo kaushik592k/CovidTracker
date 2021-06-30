47377485dc0eaeed9af91904db34b123dd3e3bf2
kaushik592k <kaushikbhartiya592k@gmail.com>
2021-07-01 01:22:51 +0530

    Frontend Styling

--- frontend/index.js
+++ frontend/index.js
@@ -1,19 +1,19 @@
 $(document).ready(function () {  
     $("#txtCountry").autocomplete({  
         source: function (request, response) {  
-            $.ajax({  
-                url: "http://localhost:8080/search",  
-                method: "post",  
-                contentType: "application/json;charset=utf-8",  
-                data: JSON.stringify({ term: request.term }),  
-                dataType: 'json',  
-                success: function (data) {  
-                    response(data.d);  
-                },  
-                error: function (err) {  
-                    alert(err);  
-                }  
-            });  
+            // $.ajax({  
+            //     url: "http://localhost:8080/search",  
+            //     method: "post",  
+            //     contentType: "application/json;charset=utf-8",  
+            //     data: JSON.stringify({ term: request.term }),  
+            //     dataType: 'json',  
+            //     success: function (data) {  
+            //         response(data.d);  
+            //     },  
+            //     error: function (err) {  
+            //         alert(err);  
+            //     }  
+            // });  
             console.log(request);
         }  
     });  
@@ -26,7 +26,7 @@ $(document).ready(function () {
         }
     });
 
-    
+
 });
 
 
